7317a298737a8d3de507289073adb912124c0f5f
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-10 15:00:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -22,7 +22,7 @@ def directors_totals(nds)
   d_index = 0
 while d_index < directors_database.length do
   d_totals = 0
-  puts directors_database[d_index][:name]
+  puts result[directors_database[d_index][:name]]
   m_index = 0
   while m_index < directors_database[d_index][:movies].length do
     #puts directors_database[d_index][:movies][m_index][:worldwide_gross]
